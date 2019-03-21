60375c1343ff1ec5da42fccaa6a191ae00d25642
Elliot svensen <elliotsvensen@hotmail.co.uk>
2019-03-21 14:45:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (musicians, instruments) {
   var musiciansPlay
   var i = 0;
   for (i = 0; i < musicians.length; i++) {
-    musiciansPlay.push((musicians[i] + " plays " + instruments[i]);
+    musiciansPlay.push((musicians[i] + " plays " + instruments[i]));
   }
   return musiciansPlay;
 }
