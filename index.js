16b3cfe46c6a4c89dd7340bcfd3e2e8276f1a43a
Elliot svensen <elliotsvensen@hotmail.co.uk>
2019-03-21 14:45:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function theBeatlesPlay (musicians, instruments) {
-  var musiciansPlay
+  var musiciansPlay = [];
   var i = 0;
   for (i = 0; i < musicians.length; i++) {
     musiciansPlay.push((musicians[i] + " plays " + instruments[i]));
